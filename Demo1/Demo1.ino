f8db686c743f30a59b90b5ef630ac8a82876fa97
Amey Kurade <ameykurade60@gmail.com>
2024-08-31 11:49:58 +0530

    With 3 LEDs

--- Demo1/Demo1.ino
+++ Demo1/Demo1.ino
@@ -1,12 +1,23 @@
 void setup() {
   // put your setup code here, to run once:
   pinMode(6, OUTPUT);
+  pinMode(5, OUTPUT);
+  pinMode(4, OUTPUT);
 }
 
 void loop() {
   // put your main code here, to run repeatedly:
   digitalWrite(6, HIGH);
-  delay(1000);
+  digitalWrite(5, LOW);
+  digitalWrite(4,LOW);
+  delay(50);
   digitalWrite(6, LOW);
-  delay(1000);
+  digitalWrite(5, HIGH);
+  digitalWrite(4, LOW);
+  delay(50);
+  digitalWrite(6, LOW);
+  digitalWrite(5, LOW);
+  digitalWrite(4, HIGH);
+  delay(50);
+  
 }
